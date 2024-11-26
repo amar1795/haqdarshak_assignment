a7b33120732272e8e95966c0b5e7725f985c690c
Amar <jeetamar496@gmail.com>
2024-11-26 15:15:23 +0530

    next config corrected

--- next.config.ts
+++ next.config.ts
@@ -7,13 +7,7 @@ const nextConfig: NextConfig = {
 export default nextConfig;
 
 module.exports = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()]
-    }
 
-    return config
-  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
